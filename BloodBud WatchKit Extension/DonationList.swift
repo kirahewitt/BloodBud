8bd24be2160de0a51d91009188af26cff644df63
Emily Gavrilenko <egavrile@calpoly.edu>
2020-11-14 16:47:05 -0800

    Display past donations on the donation list view

--- BloodBud WatchKit Extension/DonationList.swift
+++ BloodBud WatchKit Extension/DonationList.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct DonationList: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(
+        entity: Donation.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Donation.date, ascending: false)]
+    ) var donations: FetchedResults<Donation>
+
     var body: some View {
-        VStack {
-            Text("Next Donation")
-            Text("Past Donations")
+        
+        if (donations.count > 0) {  
+            let firstDate = donations[0].date    
+            let lastDonation = Calendar.current.dateComponents([.day], from: firstDate, to: Date())
+            let nextDonation = 56 - lastDonation
+            VStack {
+                Text("Last Donation")
+                HStack {
+                    Text("\(dayDiff) days ago")
+                }
+                Text("Next Donation")
+                HStack {
+                    if (nextDonation <= 0){
+                        Text("You're eligible for one today")
+                    }
+                    else {
+                        Text("\(nextDonation) days from today")
+                    }
+                }
+
+                Text("Past Donations")
+                List {
+                    ForEach(donations) { item in
+                        DonationRow(item: item)
+                    }
+                }
+            } 
+        }
+        else {
+            Text("No donations on file, save a life and sign up to donate blood today")
         }
     }
 }
